506084304568446c84f4cc3a3a83ca599abadf9a
Ignacio Quevedo <ignacioquevedo003@gmail.com>
2023-11-01 16:49:53 -0300

    [REFACTOR] Made it non nullable the property

--- FinTrac/BusinessLogic/Dto Components/UserDTO.cs
+++ FinTrac/BusinessLogic/Dto Components/UserDTO.cs
@@ -4,8 +4,7 @@ public class UserDTO
 {
 
     public string FirstName { get; set; } = "";
-    public string LastName { get; set; }
-
-    public string Email { get; set; }
+    public string LastName { get; set; } = "";
+    public string Email { get; set; } = "";
 
 }
